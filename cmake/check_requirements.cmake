64942f4e854dc691308338b5cc507464839e8a78
xuyongfei <xuyongfei@huawei.com>
2021-03-12 15:52:24 +0800

    support py3.8 and py3.9

--- cmake/check_requirements.cmake
+++ cmake/check_requirements.cmake
@@ -15,7 +15,7 @@ if(Python3_FOUND)
     message("Python3 library path: ${Python3_LIBRARY}")
     message("Python3 interpreter: ${Python3_EXECUTABLE}")
 elseif(Python3_LIBRARY AND Python3_EXECUTABLE AND
-        ${Python3_VERSION} VERSION_GREATER_EQUAL "3.7.0" AND ${Python3_VERSION} VERSION_LESS "3.9.0")
+        ${Python3_VERSION} VERSION_GREATER_EQUAL "3.7.0" AND ${Python3_VERSION} VERSION_LESS "3.10.0")
     message(WARNING "Maybe python3 environment is broken.")
     message("Python3 library path: ${Python3_LIBRARY}")
     message("Python3 interpreter: ${Python3_EXECUTABLE}")
--- setup.py
+++ setup.py
@@ -210,6 +210,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: C++',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
