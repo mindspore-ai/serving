6a93f5e56d4c2fde299f12845c14be1ac168c5d1
xuyongfei <xuyongfei@huawei.com>
2023-02-17 17:35:45 +0800

    1. Serving, 1.9, add patach for openssl, pybind, engin, protobuf, zlib 2. Fix bug when unix socket address too long 3. Support Ascend/lastest 4. Fix bug for importlib.util

--- cmake/external_libs/openssl.cmake
+++ cmake/external_libs/openssl.cmake
@@ -18,7 +18,15 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Linux" OR APPLE)
             CONFIGURE_COMMAND ./config no-zlib no-shared
             PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3711.patch
             PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3712.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-4160.patch
             PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-0778.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-1292.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2068.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2097.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4304.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
             )
     include_directories(${openssl_INC})
     add_library(mindspore_serving::ssl ALIAS openssl::ssl)
