14a69cf70b66e5fe2dc70b107245ea42f7f320b7
xuyongfei <xuyongfei@huawei.com>
2023-02-17 17:35:45 +0800

    1. Serving, 1.9, add patach for openssl, pybind, engin, protobuf, zlib 2. Fix bug when unix socket address too long 3. Fix bug for importlib.util

--- cmake/external_libs/openssl.cmake
+++ cmake/external_libs/openssl.cmake
@@ -23,6 +23,10 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Linux" OR APPLE)
             PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-1292.patch
             PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2068.patch
             PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2097.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4304.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
+            PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
             )
     include_directories(${openssl_INC})
     add_library(mindspore_serving::ssl ALIAS openssl::ssl)
