0a883118796da7a42e0ccf42eb4f28b78776a9b0
JohnYiZheng <yz3366@columbia.edu>
2023-12-05 10:20:17 +0800

    fix onsite issues, strengthen communication

--- test/st/test_continuous_batch.sh
+++ test/st/test_continuous_batch.sh
@@ -1,23 +1,23 @@
+PORT=${1}
 
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":200, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":200, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":200, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":200, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":512, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":512, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":500, "do_sample":"False", "top_k":5.0, "top_p":0.8,"return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":500, "do_sample":"False", "top_k":5.0, "top_p":0.8,"return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":300, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":300, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":100, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":100, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":300, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":300, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":256, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":256, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":600, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":600, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
-
-curl 127.0.0.1:19280/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":400, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
+curl 127.0.0.1:${PORT}/models/llama2     -X POST     -d '{"inputs":"what is Monetary Policy?","parameters":{"max_new_tokens":400, "do_sample":"False", "return_full_text":"True"}, "stream":"True"}' -H 'Content-Type: application/json' &
 
 
--- test/st/test_get.sh
+++ test/st/test_get.sh
@@ -1,14 +1,16 @@
-curl 127.0.0.1:19280/serving/get_bs     -X GET &
+PORT=${1}
 
-curl 127.0.0.1:19280/serving/get_bs     -X GET &
+curl 127.0.0.1:${PORT}/serving/get_bs     -X GET &
 
-curl 127.0.0.1:19280/serving/get_bs     -X GET &
+curl 127.0.0.1:${PORT}/serving/get_bs     -X GET &
 
-curl 127.0.0.1:19280/serving/get_bs     -X GET &
+curl 127.0.0.1:${PORT}/serving/get_bs     -X GET &
 
-curl 127.0.0.1:19280/serving/get_bs     -X GET &
+curl 127.0.0.1:${PORT}/serving/get_bs     -X GET &
 
-curl 127.0.0.1:19280/serving/get_request_numbers     -X GET
-curl 127.0.0.1:19280/serving/get_request_numbers     -X GET
-curl 127.0.0.1:19280/serving/get_request_numbers     -X GET
-curl 127.0.0.1:19280/serving/get_request_numbers     -X GET
+curl 127.0.0.1:${PORT}/serving/get_bs     -X GET &
+
+curl 127.0.0.1:${PORT}/serving/get_request_numbers     -X GET
+curl 127.0.0.1:${PORT}/serving/get_request_numbers     -X GET
+curl 127.0.0.1:${PORT}/serving/get_request_numbers     -X GET
+curl 127.0.0.1:${PORT}/serving/get_request_numbers     -X GET
