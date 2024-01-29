904fea00f807ca873a411ed3b5133bb08a178133
wangpingan2 <wangpingan2@huawei.com>
2024-01-29 15:05:13 +0800

    support internlm and baichuan2

--- mindspore_serving/models/tokenizer/__init__.py
+++ mindspore_serving/models/tokenizer/__init__.py
@@ -1,5 +1,7 @@
 """Custom tokenizer"""
 from .internlm_tokenizer import InternLMTokenizer
 from .llama_tokenizer import LlamaTokenizer
+from .baichuan2_tokenizer import Baichuan2Tokenizer
 
-__all__ = ['InternLMTokenizer', 'LlamaTokenizer']
+__all__ = ['InternLMTokenizer', 'LlamaTokenizer', 'Baichuan2Tokenizer']
+
